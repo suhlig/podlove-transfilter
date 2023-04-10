99941c7c7dc5d31a166a987e0a955898757c3c3e
Steffen Uhlig <steffen@familie-uhlig.net>
2023-04-10 19:55:25 +0200

    Remove speaker
    We don't have that information, so there is no point in generating a null value.

--- podlove/transcript.go
+++ podlove/transcript.go
@@ -9,7 +9,5 @@ type Segment struct {
 	StartMs uint64 `json:"start_ms"`
 	End     string `json:"end"`
 	EndMs   uint64 `json:"end_ms"`
-	Speaker string `json:"speaker"`
-	Voice   string `json:"voice"`
 	Text    string `json:"text"`
 }
